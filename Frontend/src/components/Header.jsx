2d5dc267b5d1e13604e2f35cb1a4273e50cc15c0
Ivana-Antonella <antonellacarmona090@gmail.com>
2023-11-22 18:53:20 -0300

    Primer commit

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from "../assets/images/Logo.png"
+
+
+export const Header = () => {
+  return (
+    <header>
+
+    <div >
+     <img src={logo} alt="Logo" className="w-32 h-32"/>
+    </div>
+
+    </header>
+  )
+}
+
+
